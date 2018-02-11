0e4369d57030bb67e052b3632bca7b6e583deaba
Luís Silva <lm2s@gmx.com>
2018-02-11 17:30:51 +0000

    Initial Commit

---
+++ NewtKit/NewtKit.h
@@ -0,0 +1,19 @@
+//
+//  NewtKit.h
+//  NewtKit
+//
+//  Created by Luís Silva on 11/02/2018.
+//  Copyright © 2018 Chipp'd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewtKit.
+FOUNDATION_EXPORT double NewtKitVersionNumber;
+
+//! Project version string for NewtKit.
+FOUNDATION_EXPORT const unsigned char NewtKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewtKit/PublicHeader.h>
+
+
